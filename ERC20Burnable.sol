72c5f9350ee46be2ca8f97ab6c757577b5cb54fa
HaomaToken <99504739+HaomaToken@users.noreply.github.com>
2022-02-13 22:50:17 +0530

    Update ERC20Burnable.sol

--- ERC20Burnable.sol
+++ ERC20Burnable.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "../ERC20.sol";
-import "../../../utils/Context.sol";
+import "https://github.com/HaomaToken/Haoma/blob/main/ERC20.sol";
+import "https://github.com/HaomaToken/Haoma/blob/main/Context.sol";
 
 /**
  * @dev Extension of {ERC20} that allows token holders to destroy both their own
